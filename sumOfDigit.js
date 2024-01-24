217888693b9380d0821384abe3bcad0880d45187
Rohit Kumre <143216315+001rohit@users.noreply.github.com>
2024-01-24 18:29:50 +0530

    Update sumOfDigit.js

--- sumOfDigit.js
+++ sumOfDigit.js
@@ -1,14 +1,14 @@
 // sum of digit 
 
-// const sum_of_digit = (num) =>{
-//     let sum = 0 ; 
-//     let digit;
-//     while(num!=0){
-//         digit = num %10;
-//         sum +=digit;
-//         num = Math.floor(num/10);
-//     }
-//     return sum;
-// }
-// const res1 =  sum_of_digit(212);
-// console.log(res1);
+const sum_of_digit = (num) =>{
+    let sum = 0 ; 
+    let digit;
+    while(num!=0){
+        digit = num %10;
+        sum +=digit;
+        num = Math.floor(num/10);
+    }
+    return sum;
+}
+const res1 =  sum_of_digit(212);
+console.log(res1);
