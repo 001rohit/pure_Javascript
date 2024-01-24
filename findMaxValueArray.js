516482f4fd3441ec7e45d81ef3432be426665a5a
Rohit Kumre <143216315+001rohit@users.noreply.github.com>
2024-01-24 18:29:03 +0530

    Update findMaxValueArray.js

--- findMaxValueArray.js
+++ findMaxValueArray.js
@@ -1,18 +1,18 @@
 // find Max Value in Array
 
-// const findMaxValueArray = (arr) =>{
-//       let max = arr[0]
-//     for(let i = 1; i<=arr.length;i++){
-//         if(arr[i]>max){
-//              max = arr[i];        
-//         }
-//     }
-//     return max;
+const findMaxValueArray = (arr) =>{
+      let max = arr[0]
+    for(let i = 1; i<=arr.length;i++){
+        if(arr[i]>max){
+             max = arr[i];        
+        }
+    }
+    return max;
 
-// }
+}
 
-// const array = [1,2,4,5,7];
+const array = [1,2,4,5,7];
 
-// const arrayMax_Value = findMaxValueArray(array);
-// console.log(arrayMax_Value);
+const arrayMax_Value = findMaxValueArray(array);
+console.log(arrayMax_Value);
 
